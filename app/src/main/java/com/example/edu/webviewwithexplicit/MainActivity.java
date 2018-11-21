6887469a10ba18a674eaea9e61e4665b03563124
in6802 <in6802@gmail.com>
2018-11-21 14:33:08 +0900

    Initial commit

---
+++ app/src/main/java/com/example/edu/webviewwithexplicit/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.edu.webviewwithexplicit;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonSearch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonSearch = findViewById(R.id.buttonSearch);
+        buttonSearch.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        EditText siteUrl = findViewById(R.id.editTextInput);
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient(){
+            //콜백함수
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
+        webView.loadUrl(siteUrl.getText().toString());
+        webView.requestFocus();
+
+        //키보드 숨기기
+        InputMethodManager inputMethodManager=(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
+    }
+}
+
